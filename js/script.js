a44cfb775176eeccb3fd09c7a8062b39f1ad145a
joxasabr05 <jaxongirmirhalikov@gmail.com>
2023-08-18 19:06:55 +0500

    Samar 10-darsJS

---
+++ js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+let number = 2;
+
+// First
+// while(number <= 22){
+//   console.log(number);
+//   number++
+// }
+
+// Second
+// do{
+//   console.log(number)
+//   number++
+// }while(number <= 3)
+
+// Third
+
+// for (let i = 0; i <= 1; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 21; i++) {
+//   if (i === 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+for (let i = 0; i <= 15; i++) {
+  if (i === 6) {
+    continue;
+  }
+  console.log(i);
+}
